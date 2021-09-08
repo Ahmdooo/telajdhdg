dd0d7bc985368838555ae01c3544a12c24e9b663
Ahmdooo <83948482+Ahmdooo@users.noreply.github.com>
2021-09-08 17:30:01 +0400

    Update pinger.bat

--- pinger.bat
+++ pinger.bat
@@ -27,49 +27,49 @@ set /p IP=Enter THE IP ADDRESS :
 echo.
 :rainbow
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 ping -t 2 0 10 127.0.0.1 >nul
 color 01
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger  )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger  )
 ping -t 2 0 10 127.0.0.1 >nul
 color 02
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 3PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 4
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 5
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 6
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 7
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 8
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 9
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 0b
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 0c
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 0d
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 0e
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 color 0f
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got Fucked With My pinger )
+IF ERRORLEVEL 1 (SET in=c & echo %IP%  Got LOLLED With My pinger )
 ping -t 2 0 10 127.0.0.1 >nul
 goto rainbow
